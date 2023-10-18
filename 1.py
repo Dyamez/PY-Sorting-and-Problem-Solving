237a6474b17d0f1ca0661948267ee774d56ed499
Dyamez <dyamez@outlook.com>
2023-10-17 19:36:18 -0700

    act

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
 # Write your solution for algorithm 1 below
+from random_list_gen import make_random_list
+
+unsorted_list = make_random_list(20)
+
+sorted_list = sorted(unsorted_list)
+
+print(unsorted_list)
+print(sorted_list)
